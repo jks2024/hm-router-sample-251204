6dde9ad4df5919adbe9e4616f036e5a7462b3d6a
jks2024 <jks2024@gmail.com>
2025-12-05 15:58:58 +0900

    sss

--- src/components/news/NewsList.js
+++ src/components/news/NewsList.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import NewsItem from "./NewsItem";
-const NewsDomain = "https://newsapi.org/v2/top-headlines?country=us";
+const NEWS_DOMAIN = "https://newsapi.org/v2/top-headlines?country=us&apiKey=";
+const APIKEY = "ffbbc82f694941a7b0e2d4f4515abcc7";
 
 const Container = styled.div`
   box-sizing: border-box;
@@ -18,7 +19,8 @@ const NewsList = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get();
+        const response = await axios.get(NEWS_DOMAIN + APIKEY);
+        setArticles(response.data.articles);
       } catch (e) {
         console.log(e);
       }
@@ -26,7 +28,14 @@ const NewsList = () => {
     fetchData();
   });
 
-  return <Container></Container>;
+  return (
+    <Container>
+      {articles &&
+        articles.map((article) => (
+          <NewsItem key={article.url} article={article} />
+        ))}
+    </Container>
+  );
 };
 
 export default NewsList;
